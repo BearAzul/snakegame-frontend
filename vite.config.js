be08327569795ab49c11b444e5ddefacda98a0d6
BearAzul <adyaabduazizulhakim@gmail.com>
2025-07-09 21:51:25 +0700

    add vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Akim Snake App",
+        short_name: "Akim Snake App",
+        description: "A Simple Snake Game Application using MERN stack",
+        theme_color: "#242842",
+        background_color: "#242842",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "192_akimsnake.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "512_akimsnake.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
