d0bd2e12649f3a1f44264362ea9471b9987d95cc
BearAzul <adyaabduazizulhakim@gmail.com>
2025-07-09 21:52:07 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { registerSW } from "virtual:pwa-register";
 import { BrowserRouter } from "react-router-dom"
+
+registerSW();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
